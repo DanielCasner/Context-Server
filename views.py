7885f26d6ff1b60f51a3d0da9c7af7c5371c44e5
Daniel Casner <daniel@anki.com>
2015-02-06 08:52:50 -0800

    Starting on views

--- views.py
+++ views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django import forms
+from django.db import models
+from pydc.context.models import *
+from datetime import *
+from decimal import Decimal
 
-def default(request):
+def debug(request):
     return HttpResponse(repr(request))
+
+def getChannels(id=None, name=None, derived=None):
+    "Return django object of all the data channels, optionally filtered"
