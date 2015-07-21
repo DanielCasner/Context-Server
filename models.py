75b17dc88a14d468d07c6d91126ab37ded3ee525
Daniel Casner <daniel@anki.com>
2015-07-21 16:01:20 -0700

    More monkeying around with model ideas. Added GeoLocation and GeoZone models. A new idea of how to do free form schema models.

--- models.py
+++ models.py
@@ -1,7 +1,35 @@
 from django.db import models
-import cPickle as pickle
+import pickle
 
-class DataChannel(models.Model):
+class RestModelBase(models.Model):
+    "A base model which includes the ability to have a schema free extension field called rest"
+    rest = models.TextField(verbose_name="JSON data.", default=pickle.dumps({}, 0),
+                            help_text="Additional fields without fixed schema")
+
+    def getRest(self):
+        return pickle.loads(self.rest)
+
+    def setRest(self, d):
+        self.rest = pickle.dumps(d, 1)
+
+    def asJSON(self):
+        d = self.getRest()
+        d.update(self.__dict__)
+        return repr(d)
+
+    def updateFromJSON(self, newData, replaceRest=False):
+        if replaceRest:
+            d = {}
+        else:
+            d = self.getRest()
+        for k, v in newData.items()
+            if k in hasattr(self, k):
+                setattr(self, k, v)
+            else:
+                d[k] = v
+        self.setRest(d)
+
+class DataChannel(RestModelBase):
     "Information about a single data source"
     chanID = models.IntegerField(primary_key=True, unique=True, verbose_name="Channel ID",
         help_text="Unique integer primary key for the channel, you can specify but it must be unique.")
@@ -11,42 +39,37 @@ class DataChannel(models.Model):
                                   default=False)
     source = models.CharField(max_length=255, verbose_name="Source code URL", blank=True,
                               help_text="Where to get source code for this channel's data generator")
-    rest = models.TextField(verbose_name="JSON data.", default=pickle.dumps({}, 0),
-                            help_text="Additional fields without fixed schema")
 
-    def getRest(self):
-        return pickle.loads(self.rest)
+class GeoZone(models.Model):
+    """A geographic zone which has been calculated from specific GeoLocations"""
+    lat = models.FloatField(verbose_name="Latitude",  help_text="Latitude of GeoZone average in degrees",
+                            null=False, editable=True)
+    lon = models.FloatField(verbose_name="Longitude", help_text="Longitude of GeoZone average in degrees",
+                            null=False, editable=True)
+    radius = models.FloatField(help_text="Radius of the zone in meters",
+                               null = False, editable=True)
 
-    def setRest(self, d):
-        self.rest = pickle.dumps(d, 2)
+    def __repr__(self):
+        return self.__dict__
 
-    def asJSON(self):
-        d = self.getRest()
-        d.update({'chanID': self.chanID, "name": self.name, "derived": self.derived, "source": self.source})
-        return repr(d)
+class GeoLocation(models.Model):
+    """Store information about the location of a data entry"""
+    lat = models.FloatField(verbose_name="Latitude",  help_text="Latitude of data point in degrees",
+                            editable=False, null=False)
+    lon = models.FloatField(verbose_name="Longitude", help_text="Longitude of data point in degrees",
+                            editable=False, null=False)
+    zone = models.ForeignKey('GeoZone', blank=True, null=True, editable=True)
 
+    def __repr__(self):
+        return self.__dict__
 
-class DataEntry(models.Model):
+class DataEntry(RestModelBase):
     """Table for storing context data, either raw or derrived.
 This table is designed for flexibility so the schema is not complete, most of the channel data is stored as JSON."""
     time = models.DateTimeField(auto_now_add=True, editable=False,
                                 verbose_name="Timestamp for this data entry")
     interval = models.TimeDeltaField(default=0, editable=False,
                                      verbose_name="Time interval width for this data entry.")
-    location = models.GeoField(blank=True, null=True, editable=False,
+    geoloc = models.ForeignKey('GeoLocation', blank=True, null=True, editable=False,
                                verbose_name="Geo-location for this data entry.")
     channel = models.ForeignKey('DataChannel', editable=False, verbose_name="Data channel")
-    
-    rest = models.TextField(verbose_name="JSON data", default=pickle.dumps({}, 0), editable=False,
-                            help_text="Non-fixed-schema data")
-
-    def getRest(self):
-        return pickle.loads(self.rest)
-
-    def setRest(self, d):
-        self.rest = pickle.dumps(d, 2)
-
-    def asJSON(self):
-        d = self.getRest()
-        d.update({"time": self.time, "channel": self.channel})
-        return
